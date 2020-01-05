c9c2327c68fb7699ab699087d5dd86610a6179cd
Ryan Doherty <rjd.atx@gmail.com>
2020-01-05 16:37:22 -0600

    2.4 exerciese - accounting for full words in Alice in Wonderland and special chars (spaces, commas, single quotes, colons, etc.)

--- src/exercises/class1/AliceInWonderland_StringIndex.java
+++ src/exercises/class1/AliceInWonderland_StringIndex.java
@@ -8,18 +8,51 @@ public class AliceInWonderland_StringIndex {
         String aliceReference = "Alice was beginning to get very tired of sitting by her sister on the bank, " +
                 "and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures" +
                 " or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversation?'";
+        int aliceEnd = aliceReference.length();
 
         System.out.println("Please enter a word: ");
         String userInput = input.nextLine();
-        if (aliceReference.toLowerCase().contains(userInput.toLowerCase())) {
-            int indexStart = aliceReference.toLowerCase().indexOf(userInput.toLowerCase());
-            int indexEnd = indexStart + userInput.length();
-            int aliceEnd = aliceReference.length();
+        if (aliceReference.toLowerCase().contains(" " + userInput.toLowerCase() + " ")) {
+            int indexStart = aliceReference.toLowerCase().indexOf(" " + userInput.toLowerCase() + " ")+1;
+            int indexEnd = indexStart + userInput.length()+1;
 
             System.out.println("The index for " + userInput + " is: " + indexStart + " and the word is " + userInput.length() + " letters long");
             System.out.println(aliceReference.substring(0, indexStart) + aliceReference.substring(indexEnd, aliceEnd));
 
-        } else {
+        }
+        else if (aliceReference.toLowerCase().contains(" " + userInput.toLowerCase() + ":"))
+        {
+            int indexStart = aliceReference.toLowerCase().indexOf(" " + userInput.toLowerCase() + ":")+1;
+            int indexEnd = indexStart + userInput.length();
+
+            System.out.println("The index for " + userInput + " is: " + indexStart + " and the word is " + userInput.length() + " letters long");
+            System.out.println(aliceReference.substring(0, indexStart-1) + aliceReference.substring(indexEnd, aliceEnd));
+        }
+        else if (aliceReference.toLowerCase().contains(" " + userInput.toLowerCase() + ","))
+        {
+            int indexStart = aliceReference.toLowerCase().indexOf(" " + userInput.toLowerCase() + ",")+1;
+            int indexEnd = indexStart + userInput.length();
+
+            System.out.println("The index for " + userInput + " is: " + indexStart + " and the word is " + userInput.length() + " letters long");
+            System.out.println(aliceReference.substring(0, indexStart-1) + aliceReference.substring(indexEnd, aliceEnd));
+        }
+        else if (aliceReference.toLowerCase().contains(" " + userInput.toLowerCase() + "?"))
+        {
+            int indexStart = aliceReference.toLowerCase().indexOf(" " + userInput.toLowerCase() + "?")+1;
+            int indexEnd = indexStart + userInput.length();
+
+            System.out.println("The index for " + userInput + " is: " + indexStart + " and the word is " + userInput.length() + " letters long");
+            System.out.println(aliceReference.substring(0, indexStart-1) + aliceReference.substring(indexEnd, aliceEnd));
+        }
+        else if (aliceReference.toLowerCase().contains("'" + userInput.toLowerCase() + " "))
+        {
+            int indexStart = aliceReference.toLowerCase().indexOf("'" + userInput.toLowerCase() + " ")+1;
+            int indexEnd = indexStart + userInput.length()+1;
+
+            System.out.println("The index for " + userInput + " is: " + indexStart + " and the word is " + userInput.length() + " letters long");
+            System.out.println(aliceReference.substring(0, indexStart) + aliceReference.substring(indexEnd, aliceEnd));
+        }
+        else {
             System.out.println("The word " + userInput + " was not found in the first sentence of Alice in Wonderland... :( ");
         }
     }
