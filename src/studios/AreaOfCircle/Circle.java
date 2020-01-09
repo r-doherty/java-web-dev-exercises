ec10bd5158f439543b1d46fa69aa35844ef75077
Ryan Doherty <rjd.atx@gmail.com>
2020-01-08 18:04:52 -0600

    semicolon fix - circle.java - line 5

--- src/studios/AreaOfCircle/Circle.java
+++ src/studios/AreaOfCircle/Circle.java
@@ -2,7 +2,7 @@ package studios.AreaOfCircle;
 
 public class Circle {
 
-    public static final double PI = 3.14
+    public static final double PI = 3.14;
 
     public static Double getArea(double radius) {
         return PI * radius * radius;
